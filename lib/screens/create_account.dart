08968b4c63793e626494d378bfee3410bfb0a9e5
sidharthV42 <sidharthv212@gmail.com>
2026-06-09 19:26:45 +0530

    Update create_account.dart

--- lib/screens/create_account.dart
+++ lib/screens/create_account.dart
@@ -1,26 +1,20 @@
-import 'package:flutter/material.dart';
+import   'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:task_management/screens/home_screen.dart';
-
 import 'login_screen.dart';
-
 class CreateAccount extends StatefulWidget {
   CreateAccount({super.key});
-
   @override
   State<CreateAccount> createState() => _CreateAccountState();
 }
-
 class _CreateAccountState extends State<CreateAccount> {
   final create_key = GlobalKey<FormState>();
   bool isEnter = true;
   bool isTick = false;
-
   late TextEditingController firstNameController;
   late TextEditingController lastNameController;
   late TextEditingController emailController;
   late TextEditingController passwordController;
-
   @override
   void initState() {
     super.initState();
@@ -29,7 +23,6 @@ class _CreateAccountState extends State<CreateAccount> {
     emailController = TextEditingController();
     passwordController = TextEditingController();
   }
-
   @override
   void dispose() {
     firstNameController.dispose();
@@ -38,7 +31,6 @@ class _CreateAccountState extends State<CreateAccount> {
     passwordController.dispose();
     super.dispose();
   }
-
   void _createAccount() {
     if (create_key.currentState!.validate()) {
       if (!isTick) {
@@ -50,16 +42,11 @@ class _CreateAccountState extends State<CreateAccount> {
         );
         return;
       }
-
       final String firstName = firstNameController.text.trim();
       final String lastName = lastNameController.text.trim();
       final String email = emailController.text.trim();
       final String password = passwordController.text.trim();
-
-      // Get the users database
       final usersBox = Hive.box("usersBox");
-
-      // Check if email already exists
       bool emailExists = false;
       for (int i = 0; i < usersBox.length; i++) {
         final user = usersBox.getAt(i);
@@ -68,7 +55,6 @@ class _CreateAccountState extends State<CreateAccount> {
           break;
         }
       }
-
       if (emailExists) {
         ScaffoldMessenger.of(context).showSnackBar(
           SnackBar(
@@ -78,43 +64,34 @@ class _CreateAccountState extends State<CreateAccount> {
         );
         return;
       }
-
-      // Save user credentials
       usersBox.add({
         'firstName': firstName,
         'lastName': lastName,
         'email': email,
         'password': password,
       });
-
       print("✅ Account created successfully!");
       print("Email: $email, Password: $password");
-
-      // Save login state
       final authBox = Hive.box("authBox");
-      authBox.put("isLoggedIn", true);
-      authBox.put("userEmail", email);
-
+      authBox.put("isLoggedIn", false);
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
           content: Text("Account created successfully!"),
           backgroundColor: Colors.green,
         ),
       );
-
       Navigator.pushReplacement(
         context,
-        MaterialPageRoute(builder: (context) => const HomeScreen()),
+        MaterialPageRoute(builder: (context) => LoginScreen()),
       );
     }
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: Padding(
-          padding: EdgeInsets.all(15),
+          padding:EdgeInsets.all(15),
           child: SingleChildScrollView(
             child: Form(
               key: create_key,
@@ -135,26 +112,23 @@ class _CreateAccountState extends State<CreateAccount> {
                       textAlign: TextAlign.left,
                     ),
                   ),
-
                   Row(
                     children: [
-                      Text("Already have an account?", style: TextStyle(color: Colors.black)),
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => LoginScreen()));
-                        },
-                        child: Text("Log in", style: TextStyle(color: Colors.deepPurple)),
+                      Text("Already have an account?",style: TextStyle(color: Colors.black)),
+                      TextButton(onPressed:(){
+                        final authBox=Hive.box("authBox");
+                        authBox.put("isLoggedIn",true);
+                        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
+                      }, child:Text("Log in",style: TextStyle(color: Colors.deepPurple))
                       )
                     ],
                   ),
-
                   Row(
                     spacing: 15,
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       Expanded(
                         child: TextFormField(
-                          controller: firstNameController,
                           validator: (String? value) {
                             if (value == null || value.isEmpty) {
                               return "First Name is required";
@@ -162,18 +136,18 @@ class _CreateAccountState extends State<CreateAccount> {
                             return null;
                           },
                           decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black45),
-                              borderRadius: BorderRadius.circular(7),
-                            ),
-                            hintText: "First Name",
-                            hintStyle: TextStyle(color: Colors.black45),
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.black45),
+                                  borderRadius: BorderRadius.circular(7)
+                              ),
+                              hintText: "First Name",
+                              hintStyle: TextStyle(color: Colors.black45),
                           ),
                         ),
                       ),
+
                       Expanded(
                         child: TextFormField(
-                          controller: lastNameController,
                           validator: (String? value) {
                             if (value == null || value.isEmpty) {
                               return "Last Name is required";
@@ -181,12 +155,12 @@ class _CreateAccountState extends State<CreateAccount> {
                             return null;
                           },
                           decoration: InputDecoration(
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black45),
-                              borderRadius: BorderRadius.circular(7),
-                            ),
-                            hintText: "Last Name",
-                            hintStyle: TextStyle(color: Colors.black45),
+                              enabledBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(color: Colors.black45),
+                                  borderRadius: BorderRadius.circular(7)
+                              ),
+                              hintText: "Last Name",
+                              hintStyle: TextStyle(color: Colors.black45),
                           ),
                         ),
                       ),
@@ -198,7 +172,7 @@ class _CreateAccountState extends State<CreateAccount> {
                       if (value == null || value.isEmpty) {
                         return "Email is required";
                       }
-                      if (!value.contains("@")) {
+                      if (!value.contains("@gmail.com")) {
                         return "Enter a valid email";
                       }
                       return null;
@@ -213,37 +187,37 @@ class _CreateAccountState extends State<CreateAccount> {
                     ),
                   ),
                   TextFormField(
-                    controller: passwordController,
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return "Password is required";
-                      }
-                      if (value.length < 6) {
-                        return "Password must be at least 6 characters";
-                      }
-                      return null;
-                    },
-                    obscureText: isEnter,
-                    decoration: InputDecoration(
-                      enabledBorder: OutlineInputBorder(
-                        borderSide: BorderSide(color: Colors.black45),
-                        borderRadius: BorderRadius.circular(7),
-                      ),
-                      hintText: "Enter your password",
-                      suffixIcon: IconButton(
-                        onPressed: () {
-                          setState(() {
-                            isEnter = !isEnter;
-                          });
-                        },
-                        icon: Icon(isEnter
-                            ? Icons.visibility_outlined
-                            : Icons.visibility_off_outlined),
-                      ),
-                      suffixIconColor: Colors.black45,
-                      hintStyle: TextStyle(color: Colors.black45),
-                    ),
+                      controller: passwordController,
+                      validator: (String? value) {
+                        if (value == null || value.isEmpty) {
+                          return "Password is required";
+                        }
+                        if (value.length < 8) {
+                          return "Password must be at least 8 characters";
+                        }
+                        return null;
+                      },       obscureText: isEnter,
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.black45),
+                            borderRadius: BorderRadius.circular(7)
+                        ),
+                        hintText: "Enter your password",
+                        suffixIcon: IconButton(
+                          onPressed: () {
+                            setState(() {
+                              isEnter = !isEnter;
+                            });
+                          },
+                          icon: Icon(isEnter
+                              ? Icons.visibility_outlined
+                              : Icons.visibility_off_outlined),
+                        ),
+                        suffixIconColor: Colors.black45,
+                        hintStyle: TextStyle(color: Colors.black45),
+                      )
                   ),
+
                   Row(
                     children: [
                       IconButton(
@@ -256,7 +230,9 @@ class _CreateAccountState extends State<CreateAccount> {
                             ? Icon(Icons.check_box_outlined, color: Colors.deepPurple)
                             : Icon(Icons.check_box_outline_blank, color: Colors.grey),
                       ),
-                      Text(" I agree to the", style: TextStyle(color: Colors.black45)),
+
+                      Text(" I agree to the", style: TextStyle(color: Colors.black45),),
+
                       TextButton(
                         onPressed: () {},
                         child: Text(
@@ -264,88 +240,86 @@ class _CreateAccountState extends State<CreateAccount> {
                           style: TextStyle(color: Colors.deepPurple),
                         ),
                       ),
+
                     ],
                   ),
+
                   InkWell(
-                    onTap: _createAccount,
+                    onTap: (){
+                      _createAccount();
+                    },
                     child: Container(
                       width: double.infinity,
                       height: 45,
                       decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(7),
-                        color: Colors.deepPurple,
+                          borderRadius: BorderRadius.circular(7),
+                          color: Colors.deepPurple
                       ),
                       child: Center(
-                        child: Text("Create Account", style: TextStyle(color: Colors.white)),
+                        child: Text("Create Account",style: TextStyle(color: Colors.white),),
                       ),
                     ),
                   ),
-                  Center(child: Text("Or register with", style: TextStyle(color: Colors.black45))),
+                  Center(child: Text("Or register with", style: TextStyle(color: Colors.black45),textAlign: TextAlign.center,)),
                   Row(
-                    spacing: 15,
-                    children: [
-                      Expanded(
-                        child: InkWell(
-                          onTap: () {
-                            print("tapped Sign in with GOOGLE");
-                            final authBox = Hive.box("authBox");
-                            authBox.put("isLoggedIn", true);
-                            authBox.put("userEmail", "google_user");
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(builder: (context) => const HomeScreen()),
-                            );
-                          },
-                          child: Container(
-                            width: double.infinity,
-                            height: 45,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(7),
-                              border: Border.all(color: Colors.black45),
-                            ),
-                            child: Center(
-                              child: Row(
-                                children: [
-                                  Image(image: AssetImage("assets/icons/google.png"), height: 60),
-                                  Text("Sign in with Google", style: TextStyle(color: Colors.deepPurple)),
-                                ],
-                              ),
-                            ),
+                spacing: 12,
+                children: [
+                  Expanded(
+                    child: InkWell(
+                      onTap: (){
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("This function not work currently!"),
+                            backgroundColor: Colors.red,
+                          ),);
+                      },
+                      child: Container(
+                        width: double.infinity,
+                        height: 45,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(7),
+                          border: Border.all(color: Colors.black45),
+                        ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image(image: AssetImage("assets/icons/google.png"),height: 24,),
+                              const SizedBox(width: 6),
+                              const Text("Google",style: TextStyle(color: Colors.deepPurple),),
+                            ],
                           ),
                         ),
                       ),
-                      Expanded(
-                        child: InkWell(
-                          onTap: () {
-                            print("tapped Sign in with APPLE");
-                            final authBox = Hive.box("authBox");
-                            authBox.put("isLoggedIn", true);
-                            authBox.put("userEmail", "apple_user");
-                            Navigator.pushReplacement(
-                              context,
-                              MaterialPageRoute(builder: (context) => const HomeScreen()),
-                            );
-                          },
-                          child: Container(
-                            width: double.infinity,
-                            height: 45,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(7),
-                              border: Border.all(color: Colors.black45),
-                            ),
-                            child: Center(
-                              child: Row(
-                                children: [
-                                  Image(image: AssetImage("assets/icons/apple.png"), height: 60),
-                                  Text("Sign in with Apple", style: TextStyle(color: Colors.deepPurple)),
-                                ],
-                              ),
-                            ),
+                    ),
+                  Expanded(
+                    child: InkWell(
+                      onTap: (){
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(
+                            content: Text("This function not work currently!"),
+                            backgroundColor: Colors.red,
+                          ),);
+                      },
+                      child: Container(
+                        width: double.infinity,
+                        height: 45,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(7),
+                          border: Border.all(color: Colors.black45),
+                        ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image(image: AssetImage("assets/icons/apple.png"),height: 24,),
+                              const SizedBox(width: 6),
+                              const Text("Apple",style: TextStyle(color: Colors.deepPurple),),
+                            ],
                           ),
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                ],
+              ),
                 ],
               ),
             ),
